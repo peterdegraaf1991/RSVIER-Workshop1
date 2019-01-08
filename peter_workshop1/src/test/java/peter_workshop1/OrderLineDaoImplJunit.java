88500cca03d0c8eda86b379490fba50eef53442c
Peter de Graaf <peterdegraaf1991@hotmail.com>
2019-01-08 19:53:06 +0100

    peter

---
+++ peter_workshop1/src/test/java/peter_workshop1/OrderLineDaoImplJunit.java
@@ -0,0 +1,14 @@
+package peter_workshop1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OrderLineDaoImplJunit {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}

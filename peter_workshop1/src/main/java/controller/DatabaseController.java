ddb333d589310aeec8555e25b84960f6e343c6eb
Peter <peterdegraaf1991@hotmail.com>
2019-01-23 11:26:55 +0100

    Added additional product to the shop

--- peter_workshop1/src/main/java/controller/DatabaseController.java
+++ peter_workshop1/src/main/java/controller/DatabaseController.java
@@ -25,7 +25,11 @@ public class DatabaseController {
 			statement.addBatch("INSERT INTO account_type (id, description) VALUES (3, 'Admin')");
 			statement.addBatch("INSERT INTO account(email,hash,customer_id,account_type_id) VALUES ('AdminAccount@email.com','sha1:64000:18:IAYKe6SfpC4B7SoMryabWYhtAjMjNG9x:M1sKuTZ01MaoxTx8vOHayGc2',1,3)");
 			statement.addBatch("INSERT INTO account(email,hash,customer_id,account_type_id) VALUES ('CustomerAccount@email.com','sha1:64000:18:IoD4nHCn8mQu/Z28nR2Y1mhvK2daEsuU:bqOXaIjMeSlbTq0y8PM66Nou',999,1)");
-			statement.addBatch("INSERT INTO product (name,price,stock) VALUES ('Bordspel',49.50,105)");
+			statement.addBatch("INSERT INTO product (name,price,stock) VALUES ('Kolonisten van Catan',49.50,100)");
+			statement.addBatch("INSERT INTO product (name,price,stock) VALUES ('Gloomhaven',199.99,5)");
+			statement.addBatch("INSERT INTO product (name,price,stock) VALUES ('Spirit Island',79.50,25)");
+			statement.addBatch("INSERT INTO product (name,price,stock) VALUES ('Ticket to Ride: Europe',40.00,80)");
+			statement.addBatch("INSERT INTO product (name,price,stock) VALUES ('Assante',19.50,40)");
 			statement.addBatch("INSERT INTO `order` (total_cost, customer_id, date) VALUES (10,1,CURRENT_TIMESTAMP)");
 			statement.addBatch("INSERT INTO order_line (order_id,product_id,amount) VALUES (1,1,20)");
 			statement.executeBatch();
